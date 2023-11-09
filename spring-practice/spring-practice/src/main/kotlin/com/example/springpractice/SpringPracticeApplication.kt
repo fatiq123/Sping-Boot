e501e10cf092a02e127e137d5cefcf4af18a2474
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-11-08 22:41:24 -0800

    get and post request in spring boot

---
+++ spring-practice/spring-practice/src/main/kotlin/com/example/springpractice/SpringPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springpractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringPracticeApplication>(*args)
+}
